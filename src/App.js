ca85a8b14a231660d2cc36763e78aa850d081622
lastbronx41 <78342713+lastbronx41@users.noreply.github.com>
2023-04-03 21:50:16 +0000

    default search change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Alice in Wonderland");
+    searchMovies("marvel");
   }, []);
 
   const searchMovies = async (title) => {
